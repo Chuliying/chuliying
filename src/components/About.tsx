1c607ae91a9e945791351d2815c3f5012ff80c33
Chuliying <chuliyinn@gmail.com>
2024-05-12 23:29:51 +0800

    [dev] update resume

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,49 +1,39 @@
 import React from 'react'
 
 const About = () => {
-    return (
-        <div>
-            <h1 className='title text-inter main-margin-lg'>ABOUT</h1>
-            <p className='main-margin-md'>
-                褚勵穎，畢業於銘傳大學商業設計系，有設計師的細膩也有工程師的邏輯。曾在新創公司工作五年，也有豐富的獨立接案經驗，擅長解決問題及跨領域溝通。
-            </p>
-            <h5 className='main-margin-sm'>
-                EXPERIENCE
-            </h5>
-            <div className='main-margin-md'>
-                <p >
-                    銘傳大學商業設計系畢業
-                    <span className="time">
-                        2012
-                    </span>
-                </p>
-                <p>
-                    威進國際 <span>網頁設計師</span><span className='time'>
-                        2014-2015
-                    </span>
-                </p>
-                <p>
-                    關鍵評論網 <span>前端工程師</span>
-                    <span className="time">
-                        2015-2020
-                    </span>
-                </p>
-                <p>
-                    青釀藝造<span>前端工程師</span>
-                    <span className="time">
-                        2021-NOW
-                    </span>
-                </p>
-            </div>
+  return (
+    <div>
+      <h1 className='title text-inter main-margin-lg'>ABOUT</h1>
+      <p className='main-margin-md'>
+        褚勵穎，畢業於銘傳大學商業設計系，擁有設計師的細膩思維以及工程師的邏輯能力。曾在新創公司工作五年，積累豐富的專業經驗，同時也有多項成功的獨立接案經歷，現任電商公司資深前端，擅長獨立解決問題，並能夠在跨領域團隊中高效溝通協作。
+      </p>
+      <h5 className='main-margin-sm'>EXPERIENCE</h5>
+      <div className='main-margin-md'>
+        <p>
+          易可思科技<span>資深前端工程師</span>
+          <span className='time'>2023-Present</span>
+        </p>
+        <p>
+          青釀藝造<span>前端工程師</span>
+          <span className='time'>2021-2023</span>
+        </p>
+        <p>
+          關鍵評論網 <span>前端工程師</span>
+          <span className='time'>2015-2020</span>
+        </p>
+        <p>
+          威進國際 <span>網頁設計師</span>
+          <span className='time'>2014-2015</span>
+        </p>
+      </div>
 
-            <h5 className='main-margin-sm'>
-                SKILLS
-            </h5>
-            <p>
-                HTML, CSS, Javascript, jQuery, React, Vue, Next.js
-            </p>
-        </div>
-    )
+      <h5 className='main-margin-sm'>SKILLS</h5>
+      <p>
+        ReactJS、NextJS、RESTFUL、GraphQL、GIT、GCP、SEO、GA、google search
+        console、Figma、Illustrator、Photoshop
+      </p>
+    </div>
+  )
 }
 
 export default About
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -2,101 +2,92 @@ import React from 'react'
 import Image from 'next/image'
 
 interface Work {
-    link: string,
-    company: string,
-    jr: string,
-    skills: string,
-    logo: string
+  link: string
+  company: string
+  jr: string
+  skills: string
+  logo: string
 }
 
 const works: Work[] = [
-    {
-        link: 'https://www.thenewslens.com/',
-        company: 'The News Lens',
-        jr: 'design,frontend',
-        skills: 'html, css, javascript, jquery',
-        logo: require('../../public/images/logo/tnl.svg')
-
-    },
-    {
-        link: 'https://everylittled.com/',
-        company: 'everylittled',
-        jr: 'design,frontend',
-        skills: 'html, css, javascript, jquery',
-        logo: require('../../public/images/logo/eld.svg')
-    },
-    {
-        link: 'https://www.tnlmedia.com/',
-        company: 'The News Lens Media Group',
-        jr: 'design,frontend',
-        skills: 'html, css, javascript, jquery',
-        logo: require('../../public/images/logo/tnl-media-group.svg')
-    },
-    {
-        link: 'https://www.toyo-autech.com.tw/web/index.html',
-        company: 'Toyo Water',
-        jr: 'design,frontend',
-        skills: 'html, css, javascript, jquery',
-        logo: require('../../public/images/logo/toyo.svg')
-    },
-    {
-        link: 'https://www.brewingarts.com.tw/',
-        company: 'Brewing Arts',
-        jr: 'design,frontend',
-        skills: 'React, Nextjs',
-        logo: require('../../public/images/logo/ba.svg')
-    },
-    {
-        link: 'https://againstagain.com/',
-        company: 'Against Again Troupe',
-        jr: 'design,frontend',
-        skills: 'React, Nextjs, graphql',
-        logo: require('../../public/images/logo/aat.svg')
-    },
+  {
+    link: 'https://www.thenewslens.com/',
+    company: 'The News Lens',
+    jr: 'design,frontend',
+    skills: 'html, css, javascript, jquery',
+    logo: require('../../public/images/logo/tnl.svg'),
+  },
+  {
+    link: 'https://everylittled.com/',
+    company: 'everylittled',
+    jr: 'design,frontend',
+    skills: 'html, css, javascript, jquery',
+    logo: require('../../public/images/logo/eld.svg'),
+  },
+  {
+    link: 'https://www.tnlmedia.com/',
+    company: 'The News Lens Media Group',
+    jr: 'design,frontend',
+    skills: 'html, css, javascript, jquery',
+    logo: require('../../public/images/logo/tnl-media-group.svg'),
+  },
+  {
+    link: 'https://www.toyo-autech.com.tw/web/index.html',
+    company: 'Toyo Water',
+    jr: 'design,frontend',
+    skills: 'html, css, javascript, jquery',
+    logo: require('../../public/images/logo/toyo.svg'),
+  },
+  {
+    link: 'https://www.brewingarts.com.tw/',
+    company: 'Brewing Arts',
+    jr: 'design,frontend',
+    skills: 'React, Nextjs',
+    logo: require('../../public/images/logo/ba.svg'),
+  },
+  {
+    link: 'https://againstagain.com/',
+    company: 'Against Again Troupe',
+    jr: 'design,frontend',
+    skills: 'React, Nextjs, Typescript, graphql',
+    logo: require('../../public/images/logo/aat.svg'),
+  },
+  {
+    link: 'https://tava-ruby.vercel.app/',
+    company: 'Tava 天使協會',
+    jr: 'frontend',
+    skills: 'React, Nextjs, Typescript, restful',
+    logo: require('../../public/images/logo/logo-wide.svg'),
+  },
 ]
 
 const Works = () => {
-    return (
-        <>
-            <div>
-                <h1 className="title main-margin-lg text-inter">
-                    WORKS
-                </h1>
-            </div>
-            <div className='works__box'>
-                <ul>
-                    {
-                        works.map((work, index: number) => (
-                            <a target='_blank' rel="noreferrer" href={work.link} key={index}>
-                                <li className='main-margin-md'>
-                                    <div className='works__des'>
-                                        <h4 className='main-margin-sm text-inter'>
-                                            {work.company}
-                                        </h4>
-                                        <p>
-                                            {work.jr}
-                                        </p>
-                                        <p className='p-2'>
-                                            {work.skills}
-                                        </p>
-                                        <span>
-                                            link to
-                                        </span>
-                                    </div>
-                                    <div className='works__image'>
-                                        <Image src={work.logo} alt={work.company} />
-
-                                    </div>
-
-                                </li>
-                            </a>
-
-                        ))
-                    }
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <h1 className='title main-margin-lg text-inter'>WORKS</h1>
+      </div>
+      <div className='works__box'>
+        <ul>
+          {works.map((work, index: number) => (
+            <a target='_blank' rel='noreferrer' href={work.link} key={index}>
+              <li className='main-margin-md'>
+                <div className='works__des'>
+                  <h4 className='main-margin-sm text-inter'>{work.company}</h4>
+                  <p>{work.jr}</p>
+                  <p className='p-2'>{work.skills}</p>
+                  <span>link to</span>
+                </div>
+                <div className='works__image'>
+                  <Image src={work.logo} alt={work.company} />
+                </div>
+              </li>
+            </a>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }
 
 export default Works
